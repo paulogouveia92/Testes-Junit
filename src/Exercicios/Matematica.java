71e03a30abc5e85d3febee6f54c79042d9ddf6a8
Paulo Gouveia <Paulo Gouveia@PauloGouveia>
2017-09-17 07:52:24 -1200

    Classe para testar

---
+++ src/Exercicios/Matematica.java
@@ -0,0 +1,26 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Paulo Gouveia
+ */
+public class Matematica {
+
+    public int Subtrair(int valor1, int valor2) {
+
+        int resultado;
+        resultado = valor1 - valor2;
+        return resultado;
+
+    }
+
+    public int Soma(int valor1, int valor2) {
+
+        int resultado;
+        resultado = valor1 + valor2;
+        return resultado;
+    }
+
+}
