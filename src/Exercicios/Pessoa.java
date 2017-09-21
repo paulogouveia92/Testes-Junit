91e077fe9ced53c31d70cc387e90c81c8666f6f7
Paulo Gouveia <Paulo Gouveia@PauloGouveia>
2017-09-20 22:52:02 -0300

    Classe pessoa

---
+++ src/Exercicios/Pessoa.java
@@ -0,0 +1,26 @@
+
+package Exercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Paulo Gouveia
+ */
+public class Pessoa {
+    
+    private String nome;
+    private int idade;
+    private char sexo;
+    private String pessoa;
+    
+    public String inseriPessoa(String nome,String sobrenome){
+        
+        
+       
+        
+        pessoa = nome +" "+sobrenome;
+        return pessoa  ;
+    }
+    
+}
